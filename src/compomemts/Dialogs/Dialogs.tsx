ef46dd6ecdcf652cbc7facd7c3af273c5fada50f
Ivan Filipchik <wot.vania@mail.ru>
2021-04-06 12:18:10 +0300

    Fix dialogs

--- src/compomemts/Dialogs/Dialogs.tsx
+++ src/compomemts/Dialogs/Dialogs.tsx
@@ -2,11 +2,29 @@ import React from 'react';
 import d from './Dialogs.module.css';
 
 const Dialogs = () => {
-    return(
-        <div>
-            Диалоги
+    return (
+        <div className={d.dialogs}>
+            <div className={d.dialogsItems}>
+                <div className={`${d.dialog} + ' " + ${d.active}`}>
+                    Дима
+                </div>
+                <div className={d.dialog}>
+                    Илья
+                </div>
+                <div className={d.dialog}>
+                    Вика
+                </div>
+                <div className={d.dialog}>
+                    Никита
+                </div>
+            </div>
+            <div className={d.messages}>
+                <div className={d.message}>Hi</div>
+                <div className={d.message}> Hello</div>
+                <div className={d.message}>Bye</div>
+            </div>
         </div>
     )
 }
 
-export  default  Dialogs;
+export default Dialogs;
