a77b588c9e297e58adddc30fe38e70759ccdb5a4
Ivan Filipchik <wot.vania@mail.ru>
2021-06-26 10:37:31 +0300

    Create LoginForm and add redux-form

--- src/compomemts/Login/Login.tsx
+++ src/compomemts/Login/Login.tsx
@@ -1,7 +1,48 @@
 import React from 'react';
+import {reduxForm, InjectedFormProps, Field} from "redux-form";
 
-const Login = (props: any) => {
-    return <div>Login beatch</div>
+type FormDataType = {
+    login: string
+    password: string
+    rememberMe: boolean
+}
+
+const LoginForm: React.FC<InjectedFormProps<FormDataType>> = (props) => {
+    return <form action="" onSubmit={props.handleSubmit}>
+        <div>
+            <Field placeholder={"Login"} name={'login'} type="text" component={'input'}/>
+        </div>
+        <div>
+            <Field placeholder={"password"} name={'password'} type="password" component={'input'}/>
+        </div>
+        <div>
+            <Field type="checkbox" name={'rememberMe'} component={'input'}/>
+            remember me
+        </div>
+        <div>
+            <button>
+                Log
+            </button>
+        </div>
+    </form>;
+}
+
+const ReduxLoginForm = reduxForm<FormDataType>({
+    form: 'login'
+})(LoginForm)
+
+const Login = () => {
+
+    const onSubmit = (formData: FormDataType) => {
+        console.log(formData)
+    }
+
+    return <div>
+        <h1>
+            Login
+        </h1>
+        <ReduxLoginForm onSubmit={onSubmit}/>
+    </div>
 }
 
 export default Login
--- src/redux/redux-store.ts
+++ src/redux/redux-store.ts
@@ -4,21 +4,23 @@ import {dialogsReducer} from "./dialogsPageReducer";
 import {usersReducer} from "./UsersReducer";
 import {authReducer} from "./authReducer";
 import thunkMiddleware from "redux-thunk";
+import {reducer as formReducer} from 'redux-form'
 
 let rootReducer = combineReducers({
-   profilePage: profileReducer,
-   dialogsPage: dialogsReducer,
-   userPage: usersReducer,
-   auth: authReducer
+    profilePage: profileReducer,
+    dialogsPage: dialogsReducer,
+    userPage: usersReducer,
+    auth: authReducer,
+    form: formReducer
 })
 declare global {
-   interface Window {
-      __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
-   }
+    interface Window {
+        __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+    }
 }
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 let store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
 
-export type ReduxStoreType= ReturnType<typeof rootReducer>
+export type ReduxStoreType = ReturnType<typeof rootReducer>
 
 export default store;
