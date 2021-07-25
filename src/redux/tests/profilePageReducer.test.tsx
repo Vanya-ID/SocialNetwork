adefc6573e6b8ecd24142b84e7d174f0c4a642ef
Ivan Filipchik <wot.vania@mail.ru>
2021-07-25 18:06:33 +0300

    profilePageReducer/ test / create test(checking the text)

--- src/redux/tests/profilePageReducer.test.tsx
+++ src/redux/tests/profilePageReducer.test.tsx
@@ -1,18 +1,26 @@
 import {addPostAC, profileReducer} from "../profilePageReducer";
 
+let state = {
+    posts: [
+        {id: 1, likeCount: 12, message: 'Hello World'},
+        {id: 2, likeCount: 12, message: 'Move Itd'}
+    ],
+    profile: null,
+    status: ''
+}
+
 it('new post should be added', () => {
     let action = addPostAC('Incubator')
-
-    let state = {
-        posts: [
-            {id: 1, likeCount: 12, message: 'Hello World'},
-            {id: 2, likeCount: 12, message: 'Move Itd'}
-        ],
-        profile: null,
-        status: ''
-    }
 
     let newState = profileReducer(state, action)
 
     expect(newState.posts.length).toBe(3)
+})
+
+it('text should be correct', () => {
+    let action = addPostAC('Incubator')
+
+    let newState = profileReducer(state, action)
+
+    expect(newState.posts[2].message).toBe('Incubator')
 })
